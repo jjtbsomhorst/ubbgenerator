9c3905246e534b2e246ee9c41ed69164abb09ef2
info@jeroensomhorst.eu <info@jeroensomhorst.eu>
2015-10-03 09:26:56 +0200

    various changes. Db support for caching images

--- MovieUbbGenerator/ README.md
+++ MovieUbbGenerator/ README.md
@@ -2,6 +2,11 @@
 
 Generates UBB code used at the Movie topic on gathering.tweakers.net
 
+## Changelog
+
+03-10-2015: Added backup of images. Refering to imdb servers returned 403's
+
+
 ## Issues
 - Uses images from 'meukje' domain. Have to be replaced by custom files
 - When adding text to review text area it's size explodes
--- MovieUbbGenerator/api.php
+++ MovieUbbGenerator/api.php
@@ -11,10 +11,51 @@ $app->get('/movies/:term',function($q)use($app){
 });
 
 $app->get('/movie/:imdb',function($q)use($app){
-	// check if in db.. and if yes return from db
 	
 	$response = file_get_contents("http://www.omdbapi.com/?type=movie&plot=full&i=".$q);
-	$app->response->setBody($response);
+	$omdbResponse= json_decode($response);
+	
+	$conn = new PDO('mysql:host=localhost;dbname=jeroen01_moviedb;charset=utf8', 'jeroen01_moviedb', 'BQJJKfgPdOkKFChp');
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	$stmt = $conn->prepare('SELECT count(*) as c from posters where imbdid = :id');
+	$stmt->bindParam(':id',$q);
+	$filename = null;
+	
+	if($stmt->execute()){
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		$count = $row['c'];
+		
+		if($count == 1){
+			$stmt = $conn->prepare('SELECT `fileid` from posters where imbdid = :id');
+			$stmt->bindParam(":id",$q);
+			if($stmt->execute()){
+				$row = $stmt->fetch(PDO::FETCH_ASSOC);
+				$filename = $row['fileid'];
+			}
+		}
+	}
+	
+	if($filename == null){
+		
+		$posterUrl = $omdbResponse->Poster;
+		$smallPosterUrl = str_replace("SX300.jpg","SX173.jpg",$posterUrl);
+		$imageData = file_get_contents($smallPosterUrl);
+		
+		$fileName = bin2hex(openssl_random_pseudo_bytes(16));
+		file_put_contents("/home/jeroen01/domains/jeroensomhorst.eu/public_html/ubbgenerator/images/".$fileName.".jpg", $imageData);
+		$stmt = $conn->prepare('INSERT INTO posters(`imbdid`,`fileid`) values(:imdb,:file)');
+		$stmt->bindParam(":imdb",$q);
+		$stmt->bindParam(":file",$fileName);
+		if($stmt->execute()){
+			$filename = $fileName;
+		}
+	}	
+	
+
+	$omdbResponse->Poster = "http://www.jeroensomhorst.eu/ubbgenerator/images/".$filename.".jpg";
+	
+	$app->response->setBody(json_encode($omdbResponse));
 	
 });
 
