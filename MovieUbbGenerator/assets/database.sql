d97e2d36dd486f50c28a7542f3aecdce993b2f23
JJTB Somhorst <j.somhorst@gmail.com>
2022-01-30 17:09:42 +0100

    skeleton symfony app

--- MovieUbbGenerator/assets/database.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `posters` (
-  `imbdid` varchar(255) NOT NULL,
-  `fileid` varchar(255) NOT NULL,
-  PRIMARY KEY (`imbdid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci'
