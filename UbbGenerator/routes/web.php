5945ec2ef0dd269d6cc2f21ed394fc9e197be7bf
jjtbsomhorst <j.somhorst@gmail.com>
2025-11-14 16:31:14 +0100

    baseline for new version

---
+++ UbbGenerator/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+require __DIR__.'/settings.php';
